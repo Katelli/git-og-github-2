db88ec6a5388f71032d0d1c4dfd93e5b146700db
Katelli <aska082024@gmail.com>
2024-08-27 12:54:47 +0200

    created a for loop that loops through my array

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ console.log("Hello, World!");
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(myArray);
+
+for (let i = 0 < myArray.length; i++; ) {
+  console.log(`Array element index position ${myArray[i]}`);
+}
